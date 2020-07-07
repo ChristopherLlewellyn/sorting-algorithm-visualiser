46870172a3202bd81a0c7ed64689282d74b87894
Chris <christerface@gmail.com>
2020-07-07 23:45:13 +0100

    Change grid square font, change font size dependent on grid size

--- sorting-algorithm-visualiser/src/components/GridSquare.vue
+++ sorting-algorithm-visualiser/src/components/GridSquare.vue
@@ -30,10 +30,69 @@ export default {
       return 500 / this.gridSize; // 500 because our target grid size is 500 x 500 pixels
     },
 
+    fontSize() {
+      let size;
+      switch(this.gridSize) {
+        case 5:
+          size = 50;
+          break;
+
+        case 6:
+          size = 45;
+          break;
+
+        case 7:
+          size = 35;
+          break;
+
+        case 8:
+          size = 32;
+          break;
+
+        case 9:
+          size = 30;
+          break;
+
+        case 10:
+          size = 27;
+          break;
+
+        case 11:
+          size = 25;
+          break;
+
+        case 12:
+          size = 23;
+          break;
+
+        case 13:
+          size = 21;
+          break;
+
+        case 14:
+          size = 19;
+          break;
+
+        case 15:
+          size = 17;
+          break;
+
+        case 16:
+          size = 16;
+          break;
+
+        default:
+          size = 14;
+          break;
+      }
+      return size;
+    },
+
     cssVars() {
       return {
         '--bg-color': this.bgColor,
-        '--size': this.size + 'px'
+        '--size': this.size + 'px',
+        '--font-size': this.fontSize + 'px'
       }
     }
   },
@@ -56,5 +115,7 @@ export default {
   line-height: var(--size);
   text-align: center;
   vertical-align: middle;
+  font-size: var(--font-size);
+  font-family: 'Source Code Pro', monospace;
 }
 </style>
